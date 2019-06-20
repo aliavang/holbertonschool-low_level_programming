d5a1f9a97676f6e91b75dd248ca3d6fdeb57523b
aliavang <714@holbertonschool.com>
2019-06-20 23:17:29 +0000

    First commit, task 8

---
+++ 0x01-variables_if_else_while/8-print-base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int base16_num;
+	char base16_alpha;
+
+	base16_num = 0;
+	while (base16_num <= 9)
+	{
+		putchar(base16_num + '0');
+		base16_num++;
+	}
+	base16_alpha = 'a';
+	while (base16_alpha <= 'f')
+	{
+		putchar(base16_alpha);
+		base16_alpha++;
+	}
+	putchar('\n');
+	return (0);
+}
