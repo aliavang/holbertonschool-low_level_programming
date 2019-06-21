e7303ade16c088ffc50ee2c7d5c23b1944e506fd
aliavang <714@holbertonschool.com>
2019-06-21 02:56:38 +0000

    First commit, task 10

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int m;
+	int n;
+
+	m = 48;
+	while (m <= 57)
+	{
+		n = 48;
+		while (n <= 57)
+		{
+			putchar(m);
+			putchar(n);
+			if (m != 57 || n != 57)
+			{
+				putchar(',');
+			}
+			n++;
+		}
+		m++;
+	}
+	putchar('\n');
+	return (0);
+}
