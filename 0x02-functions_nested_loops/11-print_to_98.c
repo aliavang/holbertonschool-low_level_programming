6ae18f03f1fd76aa7d1b15bf46ca0cdba12f2f88
aliavang <714@holbertonschool.com>
2019-06-25 05:42:53 +0000

    First commit, task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+/**
+ * print_to_98 - Print from n to 98
+ * @n: Number input
+ *
+ * Return: Void
+ */
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+			if (n > 98)
+			{
+				printf(", ");
+			}
+			n--;
+		}
+	}
+	else if (n < 98)
+	{
+		while (n <= 98)
+		{
+			printf("%d", n);
+			if (n < 98)
+			{
+				printf(", ");
+			}
+			n++;
+		}
+	}
+	else
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+}
