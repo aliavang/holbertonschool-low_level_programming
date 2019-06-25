323ee0a1f7f8aaac3b3b7b2965c49dd2257b8fa5
aliavang <714@holbertonschool.com>
2019-06-25 05:42:34 +0000

    First commit, task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * add - Add int 1 and int 2 and return the result
+ * @a: Number input 1
+ * @b: Number input 2
+ *
+ * Return: Sum of int 1 + int 2
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
