ef54385c746b233c5e2b21e884e6ed02854b4060
aliavang <714@holbertonschool.com>
2019-06-27 19:04:42 +0000

    First commit, task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * mul - Multiples two numbers together
+ * @a: First number input
+ * @b: Second number input
+ *
+ * Return: Result of multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
