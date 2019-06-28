f07f392f0284f20bd0819e6836dc347191938900
aliavang <714@holbertonschool.com>
2019-06-28 01:45:31 +0000

    First commit, task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+/**
+ * print_diagonal - Print diagonal line in terminal
+ *
+ * Return: Void
+ */
+void print_diagonal(int n)
+{
+	int loop1, loop2;
+
+	if (!(n <= 0))
+	{
+		loop1 = 0;
+		while (loop1 < n)
+		{
+			loop2 = 0;
+			while (loop2 < loop1)
+			{
+				_putchar(' ');
+				loop2++;
+			}
+			_putchar('\\');
+			_putchar('\n');
+			loop1++;
+		}
+	}
+       	else
+       	{
+       		_putchar('\n');
+       	}
+}
