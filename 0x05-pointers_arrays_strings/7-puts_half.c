c1ff647129ef12dc9ef03b422e40712373ee9d5c
aliavang <714@holbertonschool.com>
2019-07-03 01:22:13 +0000

    First commit, task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+/**
+ * puts_half - Print second half of string
+ * @str: String input
+ *
+ * Return: Void
+ */
+void puts_half(char *str)
+{
+	int len, n;
+
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	if (len % 2 != 0)
+	{
+		n = (len - 1) / 2;
+		printf("%d\n", n);
+		while (str[n] != '\0')
+		{
+			_putchar(str[n]);
+			n++;
+		}
+	}
+	else
+	{
+		len = len / 2;
+		printf("%d\n", len);
+		while (str[len] != '\0')
+		{
+			_putchar(str[len]);
+			len++;
+		}
+	}
+	_putchar('\n');
+}
