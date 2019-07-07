9a13101e2e11066184a2ffbd8e0c6d60df712203
aliavang <714@holbertonschool.com>
2019-07-07 21:43:58 +0000

    First commit, task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * _strcat - Glue two strings together, but only up to nth bytes from
+ * string 2
+ * @dest: String one
+ * @src: String 2
+ * @n: Number of bytes to glue to string one
+ *
+ * Return: Pointer to dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int dest_len, index;
+
+	dest_len = 0;
+	while (dest[dest_len] != '\0')
+	{
+		dest_len++;
+	}
+	index = 0;
+	while (src[index] != '\0' && index < n)
+	{
+		dest[dest_len] = src[index];
+		dest_len++;
+		index++;
+	}
+	return (dest);
+}
