82c1b90795dba76531253d12629c3ed5a451b40d
aliavang <714@holbertonschool.com>
2019-07-15 02:49:53 +0000

    First commit, task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: Number of arguments
+ * @argv: Array of arguements from command line
+ *
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	while (argc == 0)
+	{
+		printf("%s\n", argv[0]);
+		argc++;
+	}
+}
