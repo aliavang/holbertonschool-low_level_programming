f173d7f31a0ba4c37b955a1a7a694a648441dd4c
aliavang <714@holbertonschool.com>
2019-07-15 03:05:47 +0000

    Fixed syntax for unused argv parameter

--- 0x0A-argc_argv/holberton.h
+++ 0x0A-argc_argv/holberton.h
@@ -5,7 +5,7 @@
 
 int _putchar(char *s);
 int main(int argc, char *argv[]);
-int main(int argc, char *__attribute__((unused)) argv[]);
+int main(int argc, char __attribute__((unused)) *argv[]);
 int main(int __attribute__((unused)) argc, char *argv[]);
 
 #endif /*HOLBERTON_H*/
