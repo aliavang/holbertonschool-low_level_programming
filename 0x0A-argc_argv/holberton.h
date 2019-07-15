6f78a82b8cd42f7684f714985a90f97349fe5952
aliavang <714@holbertonschool.com>
2019-07-15 02:50:54 +0000

    Added stdio.h to header file

--- 0x0A-argc_argv/holberton.h
+++ 0x0A-argc_argv/holberton.h
@@ -1,6 +1,8 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+#include <stdio.h>
+
 int _putchar(char *s);
 int main(int argc, char *argv[]);
 int main(int argc, char *__attribute__((unused)) argv[]);
