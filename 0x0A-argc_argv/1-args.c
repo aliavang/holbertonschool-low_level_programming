d7b5f35861f1f600414f27919e372bde53560754
aliavang <714@holbertonschool.com>
2019-07-15 04:29:58 +0000

    First commit, task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * main - Entry point
+ * @argc: Number of arguments
+ * @argv: Array of arguments from command line
+ *
+ * Return: 0 (Success)
+ */
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
