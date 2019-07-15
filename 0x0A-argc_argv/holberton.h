f75e5a542ad90421f4e3f54399420b12ef169306
aliavang <714@holbertonschool.com>
2019-07-15 02:49:31 +0000

    First commit, header file

---
+++ 0x0A-argc_argv/holberton.h
@@ -0,0 +1,9 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int _putchar(char *s);
+int main(int argc, char *argv[]);
+int main(int argc, char *__attribute__((unused)) argv[]);
+int main(int __attribute__((unused)) argc, char *argv[]);
+
+#endif /*HOLBERTON_H*/
