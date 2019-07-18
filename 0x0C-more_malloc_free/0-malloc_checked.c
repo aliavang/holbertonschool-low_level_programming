0143592356b40487c160d4e3ff8d7948dee50f77
aliavang <714@holbertonschool.com>
2019-07-18 21:13:26 +0000

    First commit, task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * malloc_checked - Check if malloc was successful
+ * @b: Amount to malloc
+ *
+ * Return: Void
+ */
+void *malloc_checked(unsigned int b)
+{
+	char *p_mem;
+
+	p_mem = malloc(sizeof(unsigned int) * b);
+	if (p_mem == NULL)
+	{
+		exit(98);
+	}
+	return (p_mem);
+}
