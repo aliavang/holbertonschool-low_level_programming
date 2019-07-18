e76e1ccfe40ab05e94f649e432763191fae4cfd9
aliavang <714@holbertonschool.com>
2019-07-18 23:32:47 +0000

    First commit, task 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+/**
+ * _memset - Fill memory with a constant byte
+ * @p_mem: Memory area
+ * @b: Constant byte to fill memory area
+ * @n: Amount of bytes to fill
+ *
+ * Return: Pointer to memory area
+ */
+char *_memset(char *p_mem, char b, unsigned int n)
+{
+	unsigned int count;
+
+	count = 0;
+	while (count < n)
+	{
+		p_mem[count] = b;
+		count++;
+	}
+	return (p_mem);
+}
+/**
+ * _calloc - Allocate memory for array
+ * @nmemb: Number of elements
+ * @size: Size of array
+ *
+ * Return: Pointer to newly allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *p_arr;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	p_arr = malloc(size * nmemb);
+	if (p_arr == NULL)
+	{
+		return (NULL);
+	}
+	p_arr = _memset(p_arr, 0, nmemb);
+	return (p_arr);
+}
