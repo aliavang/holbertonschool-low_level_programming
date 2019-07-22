de26682241a666497a1d126ff462b5ed168b4df9
aliavang <714@holbertonschool.com>
2019-07-22 21:42:09 +0000

    Included main prototype, and removed 0 from header define

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,6 +1,8 @@
-#ifndef 0_OBJECT_LIKE_MACRO_H
-#define 0_OBJECT_LIKE_MACRO_H
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
 
 #define SIZE 1024
 
-#endif /*0_OBJECT_LIKE_MACRO_H*/
+int main(void);
+
+#endif /*OBJECT_LIKE_MACRO_H*/
