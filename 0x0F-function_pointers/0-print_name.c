2944e4903d2322bc750d9b60874127b95db269da
aliavang <714@holbertonschool.com>
2019-07-24 23:47:38 +0000

    Fixed formatting errors, removed s from char *s in function prototype

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,11 +4,10 @@
  * @name: String containing name
  * @f: Pointer pointing to function that will format name as is or in
  * uppercase
- * @s: String containing name passed to function pointer
  *
  * Return: Void
  */
-void print_name(char *name, void (*f)(char *s))
+void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL)
 	{
