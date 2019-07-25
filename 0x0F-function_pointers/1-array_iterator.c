018ff1f8ffb41496951e84808b97906d4aed0276
aliavang <714@holbertonschool.com>
2019-07-25 00:06:36 +0000

    First commit, task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - Print elements in array in decimal or hex
+ * depending on the function passed
+ * @array: Array of integers
+ * @size: Size of array
+ * @action: Pointer pointing to function that will print in decimal or
+ * hex
+ *
+ * Return: Void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t index;
+
+	index = 0;
+	while (index < size)
+	{
+		action(array[index]);
+		index++;
+	}
+}
