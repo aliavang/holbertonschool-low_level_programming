6566987e2f67f919c39e343060f9b56437ff7a80
aliavang <714@holbertonschool.com>
2019-07-25 00:41:48 +0000

    First commit, task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+/**
+ * int_index - Search for integer
+ * @array: Array of integers
+ * @size: Size of array
+ * @cmp: Pointer pointing to function
+ *
+ * Return: Index of integer
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int index;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	index = 0;
+	while (index < size)
+	{
+		if (cmp(array[index]) != 0)
+		{
+			return (index);
+		}
+		index++;
+	}
+	return (-1);
+}
