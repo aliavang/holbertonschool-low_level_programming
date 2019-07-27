e474a8f32f8ac85237b509c57661e3b2381878ce
aliavang <714@holbertonschool.com>
2019-07-27 21:18:53 +0000

    Added struct in header

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,9 +2,20 @@
 #define VARIADIC_FUNCTIONS_H
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <stdarg.h>
+#include <stdlib.h>
 
+/**
+ * struct format - Struct for format
+ * @format: Format specifiers
+ * @f: Function associated with format specifiers
+ */
+struct format
+{
+	char *format;
+	void (*f)(va_list);
+};
+typedef struct format format_t;
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
