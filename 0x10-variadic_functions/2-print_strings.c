06f76487628e5188bea243b734a37aad82f45b29
aliavang <714@holbertonschool.com>
2019-07-27 21:15:50 +0000

    First commit, task 2

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,40 @@
+#include "variadic_functions.h"
+/**
+ * print_strings - Print strings
+ * @separator: String to be printed between each string
+ * @n: Number of string passed
+ *
+ * Return: Void
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list string;
+	unsigned int index;
+	char *s;
+
+	va_start(string, n);
+	if (n == 0)
+	{
+		return;
+	}
+	index = 0;
+	while (index < n)
+	{
+		s = va_arg(string, char *);
+		if (s == NULL)
+		{
+			printf("(nil)");
+		}
+		else
+		{
+			printf("%s", s);
+		}
+		if (index < n - 1 && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+		index++;
+	}
+	va_end(string);
+	printf("\n");
+}
