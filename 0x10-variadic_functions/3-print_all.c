84f977d4a2d078119f6399e88a691a058b1491b4
aliavang <714@holbertonschool.com>
2019-07-27 21:16:00 +0000

    First commit, task 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,91 @@
+#include "variadic_functions.h"
+/**
+ * print_char - Print character
+ * @args: Arguments
+ *
+ * Return: Void
+ */
+void print_char(va_list args)
+{
+	printf("%c", va_arg(args, int));
+}
+/**
+ * print_int - Print integer
+ * @args: Arguments
+ *
+ * Return: Void
+ */
+void print_int(va_list args)
+{
+	printf("%d", va_arg(args, int));
+}
+/**
+ * print_float - Print float
+ * @args: Arguments
+ *
+ * Return: Void
+ */
+void print_float(va_list args)
+{
+	printf("%f", va_arg(args, double));
+}
+/**
+ * print_string - Print string
+ * @args: Arguments
+ *
+ * Return: Void
+ */
+void print_string(va_list args)
+{
+	char *s;
+
+	s = va_arg(args, char *);
+	if (s == NULL)
+	{
+		printf("(nil)");
+	}
+	else
+	{
+		printf("%s", s);
+	}
+}
+/**
+ * print_all - Print anything
+ * @format: Types of arguments passed to function
+ *
+ * Return: Void
+ */
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	unsigned int index, index2;
+	format_t formats[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_string},
+		{NULL, NULL}
+	};
+
+	va_start(args, format);
+	index = 0;
+	while (format != NULL && format[index] != '\0')
+	{
+		index2 = 0;
+		while (index2 < 4)
+		{
+			if (*formats[index2].format == format[index])
+			{
+				formats[index2].f(args);
+				if (index2 < 3)
+				{
+					printf(", ");
+				}
+			}
+			index2++;
+		}
+		index++;
+	}
+	va_end(args);
+	printf("\n");
+}
