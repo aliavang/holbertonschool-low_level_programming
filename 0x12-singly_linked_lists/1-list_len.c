3d7c9829d3d3e44d8205c263b542ce4f69e2885c
aliavang <714@holbertonschool.com>
2019-08-02 03:41:23 +0000

    First commit, task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * list_len - Print number of elements in node in linked list
+ * @h: Pointer to first node
+ *
+ * Return: Number of elements
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
