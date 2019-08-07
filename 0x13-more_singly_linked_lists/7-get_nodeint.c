c8262e17489c0022ed76bcdb8e6685fec9ddcf54
aliavang <714@holbertonschool.com>
2019-08-07 04:37:29 +0000

    First commit, task 7

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - Get node at nth index
+ * @head: Pointer to head
+ * @index: Index number
+ *
+ * Return: pointer to nth index
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *tmp;
+	unsigned int i;
+
+	tmp = head;
+	i = 0;
+	while (i < index)
+	{
+		i++;
+		tmp = tmp->next;
+	}
+	return (tmp);
+}
