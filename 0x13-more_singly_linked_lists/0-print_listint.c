1e4eaa42b8be2f6d1aa2ae5ad2be9e6b48120ad2
aliavang <714@holbertonschool.com>
2019-08-07 00:59:26 +0000

    First commit, task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * print_listint - Print all elements of list
+ * @h: Pointer to list
+ *
+ * Return: Number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count;
+
+	count = 0;
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h-> next;
+		count++;
+	}
+	return (count);
+}
