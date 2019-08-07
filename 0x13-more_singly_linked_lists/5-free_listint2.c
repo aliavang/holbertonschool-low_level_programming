10bee2de5b89a82c0ac04935b052b38587cc32e7
aliavang <714@holbertonschool.com>
2019-08-07 02:02:03 +0000

    First commit, task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_listint2 - Free list and set head to NULL
+ * @head: Pointer to pointer pointing to head
+ *
+ * Return: Void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	while (*head != NULL)
+	{
+		tmp = *head;
+		*head = (*head)->next;
+		free(tmp);
+	}
+	*head = NULL;
+}
