2c9a572ab8a5b9724c0802a8b49a0891f3ae304a
aliavang <714@holbertonschool.com>
2019-08-08 03:54:40 +0000

    Re-commit, changed variable name of hold to current; line 37

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -34,7 +34,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 	current = tmp;
 	current = current->next;
-	tmp->next = hold->next;
+	tmp->next = current->next;
 	free(current);
 	return (1);
 }
