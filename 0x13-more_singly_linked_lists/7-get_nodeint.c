220ede3f0c3c3fb97704574ac55930d6b0fb1e31
aliavang <714@holbertonschool.com>
2019-08-08 03:00:13 +0000

    Added condition for head == NULL and index != i

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,12 +11,20 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *tmp;
 	unsigned int i;
 
+	if (head == NULL)
+	{
+		return (NULL);
+	}
 	tmp = head;
 	i = 0;
-	while (i < index)
+	while (tmp != NULL && i < index)
 	{
 		i++;
 		tmp = tmp->next;
+	}
+	if (index != i)
+	{
+		return (NULL);
 	}
 	return (tmp);
 }
