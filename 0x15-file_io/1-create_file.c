f1856320a098820f538c5f9933e01a4c70919e46
aliavang <714@holbertonschool.com>
2019-08-14 06:10:37 +0000

    First commit, task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+/**
+ * create_file - Create a file
+ * @filename: Name of file to create
+ * @text_content: String to put inside file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, w, len;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		len = 0;
+		while (text_content[len] != '\0')
+		{
+			len++;
+		}
+		w = write(fd, text_content, len);
+		if (w == -1)
+		{
+			return (-1);
+		}
+	}
+	close (fd);
+	return (1);
+}
