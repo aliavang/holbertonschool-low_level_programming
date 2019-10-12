9c6d1227d87bacb7424442ff823d8410bdc56d75
aliavang <714@holbertonschool.com>
2019-10-12 00:51:23 +0000

    Malloced accordingly, once for array, once for nodes, set all nodes to NULL

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,11 +8,29 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_hash;
+	unsigned long int i;
 
-	new_hash = malloc(sizeof(unsigned long int) * size);
+	if (size < 0)
+	{
+		return (NULL);
+	}
+	new_hash = malloc(sizeof(hash_table_t));
 	if (new_hash == NULL)
 	{
 		return (NULL);
+	}
+	new_hash->size = size;
+	new_hash->array = malloc(sizeof(hash_node_t));
+	if (new_hash->array == NULL)
+	{
+		free(new_hash);
+		return (NULL);
+	}
+	i = 0;
+	while (i < size)
+	{
+		new_hash->array[i] = NULL;
+		i++;
 	}
 	return (new_hash);
 }
