d9d1cf29a5aa56c67cccc05d7267fffdc646acc3
aliavang <714@holbertonschool.com>
2019-12-07 03:30:12 +0000

    Added if condition for left and right and changed equation for leaves

--- 0x1D-binary_trees/12-binary_tree_leaves.c
+++ 0x1D-binary_trees/12-binary_tree_leaves.c
@@ -14,6 +14,10 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	{
 		return (0);
 	}
-	l = 1 + binary_tree_leaves(tree->left);
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (1);
+	}
+	l = binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
 	return (l);
 }
