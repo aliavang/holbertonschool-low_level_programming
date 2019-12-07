a3b57b033696a5b7f22f6bb791e28e7e78a81770
aliavang <714@holbertonschool.com>
2019-12-07 01:52:33 +0000

    First commit, task 8

---
+++ 0x1D-binary_trees/8-binary_tree_postorder.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_psotorder - Go through binary tree using post-order traversal
+ * @tree: Pointer to root of tree
+ * @func: Print function from main
+ *
+ * Return: Void
+ */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+	{
+		return;
+	}
+	binary_tree_postorder(tree->left, (*func));
+	binary_tree_postorder(tree->right, (*func));
+	(*func)(tree->n);
+}
