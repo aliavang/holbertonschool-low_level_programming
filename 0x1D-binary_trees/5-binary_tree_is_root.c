545136a164e75c1ed1ce67acbb68c23eadede886
aliavang <714@holbertonschool.com>
2019-12-07 01:23:04 +0000

    First commit, task 5

---
+++ 0x1D-binary_trees/5-binary_tree_is_root.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_root - Check if node is root
+ * @node: Node to be checked
+ *
+ * Return: 1 if root otherwise 0
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node == NULL)
+	{
+		return (0);
+	}
+	if (node->parent == NULL)
+	{
+		return (1);
+	}
+	return (0);
+}
