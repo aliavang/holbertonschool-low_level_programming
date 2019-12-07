5d7ec098f14765a96a560982a44bb4890b12bdcf
aliavang <714@holbertonschool.com>
2019-12-07 02:15:39 +0000

    Added space around addition operative; ln 20

--- 0x1D-binary_trees/9-binary_tree_height.c
+++ 0x1D-binary_trees/9-binary_tree_height.c
@@ -17,7 +17,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	}
 	if (tree->left != NULL)
 	{
-		left_h = 1+ binary_tree_height(tree->left);
+		left_h = 1 + binary_tree_height(tree->left);
 	}
 	if (tree->right != NULL)
 	{
