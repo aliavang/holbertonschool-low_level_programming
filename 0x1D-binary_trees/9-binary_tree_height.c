e00d9ce1aa64f7b3df848319a61fe39267cb2742
aliavang <714@holbertonschool.com>
2019-12-07 02:14:33 +0000

    First commit, task 9

---
+++ 0x1D-binary_trees/9-binary_tree_height.c
@@ -0,0 +1,35 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_height - Measure height of binary tree
+ * @tree: Pointer to root node of tree
+ *
+ * Return: Height of tree otherwise 0
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_h, right_h;
+
+	left_h = 0;
+	right_h = 0;
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	if (tree->left != NULL)
+	{
+		left_h = 1+ binary_tree_height(tree->left);
+	}
+	if (tree->right != NULL)
+	{
+		right_h = 1 + binary_tree_height(tree->right);
+	}
+	if (left_h > right_h)
+	{
+		return (left_h);
+	}
+	else
+	{
+		return (right_h);
+	}
+	return (0);
+}
